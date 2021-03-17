1d0746ff20a5631818d443503be17b6ee52f326d
ilya yasiuk <iyasiuk@elinext.com>
2021-03-17 15:36:15 +0300

    adding call to heroku

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,19 @@
-setInterval(()=>{
-	console.log('call');
-}, 3000)
+window.onload = (event) => {
+  console.log('page is fully loaded');
+   timeout = setInterval(() =>{
+	  axios.get('https://guarded-reaches-84708.herokuapp.com/version')
+	    .then(function (response) {
+	      // handle success
+	      console.log('call guarded-reaches-84708');
+	      const apiVersion = response.data.version;
+	      console.log(apiVersion);
+	    })
+	    .catch(function (error) {
+	      // handle error
+	      console.log(error);
+	    })
+	    .then(function () {
+	      // always executed
+	    });
+	}, 10*1000)
+};
